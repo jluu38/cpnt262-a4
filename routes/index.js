40f6e12da091b7ab68ea553a42ed4c0b04a835fe
unknown <jessica.luu@edu.sait.ca>
2021-03-20 14:07:51 -0600

    module added to views now working

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,45 @@
 const express = require("express");
 const Router = express.Router();
-// const ejsLint = require("ejs-lint");
-// ejsLint()
 
+const randomColor = require("randomcolor");
+let color = randomColor();
 
+Router.use(function (request, response, next) {
+  response.locals = {
+    siteTitle: "100 Cinema",
+    tagline: "One auditorium. One hundred seats.",
+    copyright: "&copy; 2021 Jess Luu. MIT License."
+  }
+  next();
+})
 
-Router.use(function(request, response, next) {
-    response.locals = {
-      siteTitle: "100 Cinema",
-      tagline: "100 seats.",
-      copyright: "&copy; 2021 Jess Luu. MIT License."
-    }
-    next();
-  })
+Router.get("/", function (request, response) {
+  response.render("pages/index", {
+    pageTitle: "Coming Soon.",
+    color: color
+  });
+})
 
-  Router.get("/", function(request, response) {
-    response.render("pages/index", {pageTitle: "Coming Soon."});
-  })
+Router.get("/comingsoon", function (request, response) {
+  response.render("pages/comingsoon", {
+    pageTitle: "Coming Soon.",
+    color: color
+  });
+})
 
-  Router.get("/comingsoon", function(request, response) {
-    response.render("pages/comingsoon", {pageTitle: "Coming Soon."});
-  })
+Router.get("/login", function (request, response) {
+  response.render("pages/login", {
+    pageTitle: "Login.",
+    color: color
+  });
+})
 
-  Router.get("/login", function(request, response) {
-    response.render("pages/login", {pageTitle: "Login."});
-  })
+Router.get("*", function (request, response) {
+  response.status(404);
+  response.render("pages/404", {
+    pageTitle: "404 Error.",
+    color: color
+  });
+})
 
-  Router.get("*", function(request, response) {
-      response.status(404);
-      response.render("pages/404", {pageTitle: "404 Error."});
-    })
-
-  module.exports = Router;
+module.exports = Router;
